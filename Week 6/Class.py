ea8ccf1f6e62e6f7efc750b1c582e362d0219647
Jasmin <2946547864@qq.com>
2025-10-02 22:26:47 +0000

    Week 6 Class 2

--- Week 6/Class.py
+++ Week 6/Class.py
@@ -21,9 +21,39 @@ print(every_word(sentence))
 
 
 #simplified version: string.split(separator, max_split_times)
+'''
 def every_word(sentence):
   return sentence.split()   #string.split(separator, max_split_times)
 
 sentence = input('Enter your sectence:')
 
 print(every_word(sentence))
+'''
+
+
+
+'''
+def every_word(sentence):
+  word = []
+  collection = ''
+  vowel = 0
+  for x in sentence:
+    if x != ' ':
+      collection += x
+      if x in 'aeiou':
+        vowel += 1
+    else:
+      if vowel >= 2:
+        word.append(collection)
+      collection = ''
+      vowel = 0
+
+  if collection != '' and vowel >=2:    #for last word
+    word.append(collection)
+
+  return word
+
+sentence = input('Enter your sectence:')
+
+print(every_word(sentence))
+'''
