48733ab6d4de635bafa37fd553f87779d64e3baf
Jasmin <2946547864@qq.com>
2025-10-05 08:15:14 +0000

    Lists practice

--- Week 6/Quiz.py
+++ Week 6/Quiz.py
@@ -74,6 +74,18 @@ print(hailstone_seq(number))
 
 
 
+#6
+'''
+def find_factors(num):
+  return [x for x in range(1,num+1) if num % x == 0]
+
+num = int(input('Enter number: '))
+
+print(find_factors(num))
+'''
+
+
+
 
 #7
 '''
@@ -138,6 +150,9 @@ cards = ['a',5,5,2,6,2,3,8,9,7]
 print(count(cards))
 '''
 
+
+
+
 #10
 '''
 def war_of_number(number):
@@ -153,37 +168,184 @@ def war_of_number(number):
 number=[2,8,7,5]
 print(war_of_number(number))
 '''
+#10 simplified version：sum
+'''
+def war_of_number(number):
+  odd = sum(x for x in number if x%2==1)
+  even = sum(x for x in number if x%2==0)
+  return 'odds' if odd > even else 'evens'
+
+number=[12,90,75,1,1]
+print(war_of_number(number))
+'''
+
+
+
+
+#11
+'''
+def add_list(lyst1,lyst2):
+  lyst3 = []
+  for x in range(0,):  #the item amount of list
+    lyst3.append(lyst1[x] + lyst2[x])
+  return lyst3
+
+lyst1 = [1,3,3,1]
+lyst2 = [4,3,6,1]
+print(add_list(lyst1,lyst2))
+'''
+
+
+
+
+#12
+'''
+def largest_even(number):
+  largest = None
+  for x in number:
+    # None, True, False: use 'is' (except assignment)
+    if x % 2 == 0 and (largest is None or x > largest):   # notice: largest is None
+      largest = x
+  return -1 if largest is None else largest
+
+number = [3,7,2,1,7,9,10,13]
+
+print(largest_even(number))
+'''
 
 
 
 
 #13
-def largest_even(number):
-  largest = 0
-  for x in number:
-    if x % 2 == 0 and x > largest:
-      largest = x
-  return -1 if largest == '' else {largest}
-
-number = [3,7,2,1,7,9,10,13]
-
-print(largest_even(number))
-
-
-
-
-#19
 '''
-def is_acronym(s,word):
-  if len(s) != len(word):
+def largest_odd(number):
+  largest = None
+  for x in number:
+    if x %2 == 1 and (largest is None or x > largest):
+      largest = x
+  return -1 if largest is None else largest
+
+number = [2,4,6,8]
+print(largest_odd(number))
+'''
+
+
+
+
+#14
+'''
+def progress_days(mylist):
+  count = 0
+  for x in range(1,len(mylist)):
+    if mylist[x] > mylist[x-1]:
+      count += 1
+  return count
+
+mylist = [3,4,1,2]
+print(progress_days(mylist))
+'''
+
+
+
+
+#15
+'''
+def lag_days(mylist):
+  count = 0
+  for x in range(1,len(mylist)):
+    if mylist[x] < mylist[x-1]:
+      count += 1
+  return count
+
+mylist = [5,3,2,1]
+print(lag_days(mylist))
+'''
+
+
+
+
+#16
+'''
+def like_or_dislike(button):
+  state = None
+  for x in button:
+    if x != state or state is None:
+      state = x
+    elif x == state:
+      state = 'nothing'
+  return state
+
+button = ['like','like']
+print(like_or_dislike(button))
+'''
+
+
+
+
+#17
+'''
+def get_indices(mylist,item):
+  result = []
+  for x in range(0,len(mylist)):
+    if str(mylist[x]) == item:    #turn to string
+      result.append(x)
+  return result
+
+mylist = [1,5,5,2,7]
+item = input('Enter item: ')
+print(get_indices(mylist,item))
+'''
+
+
+
+
+#18
+'''
+def list_of_multiples(num,length):
+  result = []
+  for x in range(1,length+1):
+    result.append(num * x)
+  return result
+
+num = int(input('Enter number: '))
+length = int(input('Enter length: '))
+print(list_of_multiples(num,length))
+'''
+
+
+
+
+#19 Solution path 1:
+'''
+def is_acronym(words,acronym):
+  words_clean = []
+
+  if len(words) != len(acronym):    # avoid ''
     return False
 
-  for i in range(0,len(s)):    # get the index max amount
-    if s[i] != word[i][0]:
-       #compare one by one
-      return False  
-  return True 
-s = input('Enter the acronym: ')
-word = ['apple','pear','cat']
-print(is_acronym(s,word))
+  else:
+    for x in range (0,len(words)):
+      if acronym[x] != words[x][0]:   #notice: words[x][0]
+        return False
+  return True
+
+words = ['alice','bob','charlie']
+acronym = input('Enter acronym: ')
+print(is_acronym(words,acronym))
+'''
+#19 Solution path 2:
+'''
+def is_acronym(words,acronym):
+  words_acronym = ''
+
+  if len(words) != len(acronym):
+    return False
+  else:
+      for x in words:
+        words_acronym += x[0]
+  return False if acronym != words_acronym else True
+
+words = ['alice','bob','charlie']
+acronym = input('Enter acronym: ')
+print(is_acronym(words,acronym))
 '''
